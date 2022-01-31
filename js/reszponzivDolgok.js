5ea7e4c0e798b6da147e8e4c13cc696d19456ff1
Csaba19999 <menesi777@gmail.com>
2022-01-31 11:17:29 +0100

    hamburger menü fixálva. gomb helyezkedése, menü megjelenése. javascrit hiba javítása

--- js/reszponzivDolgok.js
+++ js/reszponzivDolgok.js
@@ -1,13 +1,7 @@
 $(function () {
-
-  //Kérlek ne másologasd az animációt kezelő funckciot egy olyan fájlba aminek az a neve hogy reszponziveDolgok mappa szerkezet nélkül.
-
-
-  const reszponzivMenuIkon = document.querySelector(".hamburgerMenu");
-
-  const navbar = document.querySelector(".navbar");
-
-  reszponzivMenuIkon.addEventListener("click", () => {
+  const reszponzivMenuIkon = $(".hamburgerMenu");
+  const navbar = $(".navbar");
+  reszponzivMenuIkon.on("click", () => {
     navbar.classList.toggle("gombValtozas");
   });
 });
