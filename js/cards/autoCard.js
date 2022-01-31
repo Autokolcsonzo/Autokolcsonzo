b29b12aff6a1053dbd3a07509bbb83d4f7354409
Csaba19999 <menesi777@gmail.com>
2022-01-31 10:07:17 +0100

    autok listázása ajaxal. animációk implemetálása és event listenerek / triggerek beépítése és felkészítése

--- js/cards/autoCard.js
+++ js/cards/autoCard.js
@@ -1,8 +1,5 @@
-$(function () {
-    $(".card-block-3,.card-block-5").css("display", "none");
-    kartyaNyito();
-    kartyaHover();
-    function kartyaNyito() {
+class autoCard{
+    kartyaNyito() {
         let zarva = false;
         $(".jarmu-card-reszletek").on("click", () => {
             if (zarva) {
@@ -15,18 +12,12 @@ $(function () {
         });
     }
 
-    function kartyaHover() {
+    kartyaHover() {
         $(".jarmu-card").on("mouseenter", () => {
             $(".jarmu-card-img-box img").css("transform", "scale(1.2)");
-            $(".jarmu-card-img-box img").css("rotate", "(2deg)");
         });
         $(".jarmu-card").on("mouseleave", () => {
             $(".jarmu-card-img-box img").css("transform", "scale(1.0)");
-            $(".jarmu-card-img-box img").css("rotate", "(0deg)");
         });
     }
-
-
-
-
-});
+}
