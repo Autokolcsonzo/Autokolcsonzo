6fc6f2fcd2e5553bf303d81834d272826fb8d920
lejlailles <il970322@gmail.com>
2022-02-03 11:24:53 +0100

    felhasználói módosítás js-el megjelenés+mappaszerkezetek

---
+++ js/felhasznalo/modosit.js
@@ -0,0 +1,9 @@
+$(function () {
+$('#fadatokMod').on('click', ()=>{
+$(".felhasznaloiModositas").slideDown(500);
+});
+
+$('#fkepMod').on('click', ()=>{
+    $("#profkepFel").slideDown(500);
+    });
+});
